f431a0357ac47c156dc0d54510ca837460fbf94c
YNavadeep <161842534+YNavadeep@users.noreply.github.com>
2024-09-27 10:34:17 +0530

    Update primenumbers.py

--- primenumbers.py
+++ primenumbers.py
@@ -1,10 +1,1 @@
-l=int(input("enter lower number"))
-u=int(input("enter upper number"))
-print("the prime numbers between ",l,"and",u,"are :")
-for n in range(l,u+1):
-    if n>1:
-        for a in range(2,n):
-            if (n%a==0):
-                break
-            else:
-                print(n)
+print("hi...")
